e8bbc530fb22441ceb46fa356d2d2a70ef045b25
Ruangrith Rithruangdech <ruangrith.ri@gmail.com>
2020-11-25 16:25:43 +0700

    UPdate

--- LidarCAR/Main/LidarBot+Compass - Clean/src/main.cpp
+++ LidarCAR/Main/LidarBot+Compass - Clean/src/main.cpp
@@ -1,5 +1,16 @@
 #include "FRAM_18_Car.hpp"
 
+void compassService(void* pvParameters) {
+  compass.init();
+  compass.setCalibration(-1270, 865, -1193, 885, -88, 93);
+  compass.setSmoothing(10, true);
+
+  for (;;) {
+    displayCompassInfo();
+    vTaskDelay(100);
+  }
+}
+
 void setup() {
   M5.begin();
 
@@ -20,26 +31,36 @@ void setup() {
   //! Motor
   lidarcar.Init();
 
-  compass.init();
+  // compass.init();
+  // compass.setCalibration(-1270, 865, -1193, 885, -88, 93);
+  // compass.setSmoothing(10, true);
 
-  // compass.setCalibration(-425, 1418, -1265, 566, -1, 1856);
-  // compass.setCalibration(-435, 1448, -1308, 586, -1, 1748);
-  // compass.setCalibration(-473, 1473, -1256, 596, -1, 1756);
-  // compass.setCalibration(-561, 1495, -1342, 656, -1, 1780);
-  // compass.setCalibration(-547, 1483, -1337, 620, -1, 1817);
-
-  // compass.setCalibration(-1, 596, -1, 581, -1, 1725);
-
-  // compass.setCalibration(-670, 1547, -1405, 778, -1, 1610);
-  compass.setCalibration(-1270, 865, -1193, 885, -88, 93);
-
-  compass.setSmoothing(10, true);
+  xTaskCreatePinnedToCore(compassService, "CompassService", 6000, NULL, 0, NULL,
+                          1);
 
   beepBeep();
 }
 
 void loop() {
   lidarcar.MapDisplay();
-  displayCompassInfo();
+  lidarcar.GetData();
+  // displayCompassInfo();
   modeSelection();
+
+  M5.Lcd.setCursor(0, 60, 2);
+  M5.Lcd.print("Azimuth : ");
+  M5.Lcd.print(azimuth16);
+  M5.Lcd.print("     ");
 }
+
+/************************************************************************/
+
+// compass.setCalibration(-425, 1418, -1265, 566, -1, 1856);
+// compass.setCalibration(-435, 1448, -1308, 586, -1, 1748);
+// compass.setCalibration(-473, 1473, -1256, 596, -1, 1756);
+// compass.setCalibration(-561, 1495, -1342, 656, -1, 1780);
+// compass.setCalibration(-547, 1483, -1337, 620, -1, 1817);
+
+// compass.setCalibration(-1, 596, -1, 581, -1, 1725);
+
+// compass.setCalibration(-670, 1547, -1405, 778, -1, 1610);
---
+++ PC/Main/Solver/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+7
+gradlew,5\b\5bbfa66edb4db3c7c33c5181f43510990d3307f9
+;
+gradlew.bat,2\a\2a45a911a8f1836b0b6c5b758962572012d8f8c3
+?
+settings.gradle,0\5\05efc8b1657769a27696d478ded1e95f38737233
+`
+0src/main/java/application/applet/MainApplet.java,d\b\db185c455402d00d5442b6c1c42315f19cc57428
+\
+,src/main/java/application/dataType/Cell.java,9\4\94c7f6e4b1435f569a36e28ba092af9e6786d606
+S
+#src/main/java/application/Main.java,5\6\5688ea0c4a98f9f292a9a6d1f18b03db74a806d0
